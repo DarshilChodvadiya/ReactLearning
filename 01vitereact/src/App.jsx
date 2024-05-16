fc15d42101bdf432c262fc92435834e6b34e9161
Darshil Chodvadiya <darshilpatel200703@gmail.com>
2024-05-16 12:53:21 +0530

    project 2

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,10 +2,15 @@ import Chai from "./chai"
 
 function App() {
 
+  const username = "react"
 
   return (
   //  <h1>Hello</h1>
+  <>
+
   <Chai/>
+  <h1>chai aur code {username}</h1>
+  </>
   )
 }
 
